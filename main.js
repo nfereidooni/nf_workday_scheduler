60882b72110282a628c7d7553f20d13cdde278e9
Niki Fereidooni <nikifereidooni@gmail.com>
2020-10-15 10:30:54 -0400

    Added Assets and Instruction Files

---
+++ main.js
@@ -0,0 +1,16 @@
+// Current Date
+
+let currentDate = moment().format('dddd, MMMM Do');
+// $("#currentDay").text(currentDate);
+// currentDay = document.getElementById("currentDay");
+console.log(currentDate)
+// currentDay.innerHTML = currentDate
+
+
+// Current Hour
+
+let currentHour = parseInt(m().format("H"));
+
+// Array for Events
+
+let savedEvents = [];
